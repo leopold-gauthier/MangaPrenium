e39c64fa3fd050633fe07e611f94962b6dc1b860
leopold.gth <121091513+leopold-gauthier@users.noreply.github.com>
2023-06-05 23:52:46 +0200

    base

---
+++ MangaPrenium/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once("./inc/include_head.php") ?>
+    <title>MangaPrenium</title>
+</head>
+<body>
+    <header><?php include_once("./inc/include_nav.php") ?></header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
---
+++ MangaPrenium/library.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once("./inc/include_head.php") ?>
+    <title>Bibliotéque</title>
+</head>
+<body>
+<header><?php include_once("./inc/include_nav.php") ?></header>
+    <main>
+        <div id="library">
+            <div class="library">
+                <div class="manga">
+                    <a href="details.php">
+                    </a>    
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+</html>
